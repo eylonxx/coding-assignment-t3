7831c002aa6d5e633b44aa815247454a7ccca27e
Eylon <eylon.perets@gmail.com>
2023-05-24 17:21:44 +0300

    Login screen responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,16 +42,16 @@ const AuthShowcase: React.FC = () => {
     <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 bg-darkPurple">
       <div className="flex h-2/5 w-1/3 flex-col justify-between rounded-xl bg-white px-2 py-10 uppercase shadow-2xl">
         <div className="flex flex-col">
-          <div className="mb-10 flex w-full items-center justify-center text-center text-xl font-semibold text-lightPurple lg:text-3xl">
+          <div className="mb-10 flex w-full items-center justify-center px-2 text-center text-lg font-semibold text-lightPurple lg:text-3xl">
             Coding Assignment by Eylon
           </div>
-          <div className="mx-auto font-semibold text-lightPurple">
+          <div className="mx-auto text-center font-semibold text-lightPurple">
             A fullstack todolist application
           </div>
         </div>
         <div className="flex flex-col gap-2">
           <button
-            className="mx-auto mt-auto w-[80%] rounded-full bg-lightPurple px-10 py-3 text-lg font-semibold uppercase text-white no-underline transition hover:bg-lightPurple/80"
+            className="mx-auto mt-auto w-[80%] rounded-lg bg-lightPurple text-center text-sm font-semibold uppercase text-white no-underline transition hover:bg-lightPurple/80 md:px-10 md:py-3 lg:text-lg"
             onClick={() => {
               void signIn("google", {
                 callbackUrl: "/home",
